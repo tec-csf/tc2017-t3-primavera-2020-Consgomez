a273f653d8c9c7212e52ea32fce7de1eaf340939
Consgomez <a01026717@itesm.mx>
2020-04-15 22:22:09 -0500

    Hasta 9999

--- sources/MultiEnteros.cpp
+++ sources/MultiEnteros.cpp
@@ -8,36 +8,38 @@ using namespace std;
 class MultiEnteros{
     public:
     int n, s;
-    int w, x, y, z;
+    int a1, a2, b1, b2;
     int pot;
-    double r, p, q;
-    //int i=0;
+    int w, x, y, z;
+    int aux;
     int resultado;
-    string strA;
+    string strA, strB;
 
     int multiplicacion(int a, int b){
-        //i++;
-        //cout<<i<<endl;
         strA = to_string(a);
+        strB = to_string(b);
         strA = resize(strA);
+        strB = resize(strB);
         n = strA.size();
-        //cout<<"n: "<<n<<endl;
-        if(n<=2){
+        if(pequeño(strA, strB) == true){
             return a*b;
-        } else if (n>2){
+        } else{
             s = n/2;
             pot = pow(10, s);
-            w = a/pot;
-            x = a%pot;
-            y = b/pot;
-            z = b%pot;
-            r = multiplicacion(w+x, y+z);
-            //cout<<"r: "<<r<<endl;
-            p = multiplicacion(w, y);
-            //cout<<"p: "<<p<<endl;
-            q = multiplicacion(x, z);
-            //cout<<"q: "<<q<<endl;
-            return ((p*pow(10, 2*s)) + ((r-p-q)*pow(10, s)) + q);
+            a1 = a/pot;
+            a2 = a%pot;
+            b1 = b/pot;
+            b2 = b%pot;
+            w = multiplicacion(a1, b1);
+            x = multiplicacion(a1, b2);
+            y = multiplicacion(a2, b1);
+            z = multiplicacion(a2, b2);
+            aux = x+y;
+            w = w * pow(10, 2*s);
+            aux = aux * pow(10, s);
+            resultado = w + aux;
+            resultado = resultado + z;
+            return resultado;
         }
         return 0;
     }
@@ -46,15 +48,22 @@ class MultiEnteros{
         string temp = "0";
         while(mayor.size()%2!=0){
             mayor.insert(0, temp);
-            //cout<<mayor<<"."<<endl;
         }
         return mayor;
+    }
+
+    bool pequeño(string a, string b){
+        if((a.size()<=2)&&(b.size()<=2)){
+            return true;
+        }else
+            return false;
+        return false;
     }
 };
 
 int main(){
-    int a = 8910;
-    int b = 8911;
+    int a = 9999;
+    int b = 9999;
     int resultado;
     MultiEnteros m;
     resultado = m.multiplicacion(a, b);
