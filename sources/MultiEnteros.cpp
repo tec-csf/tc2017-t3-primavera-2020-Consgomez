da00a91798d7ec65a6e08a630b5f0bcd39c08846
Consgomez <a01026717@itesm.mx>
2020-04-15 21:57:07 -0500

    Hasta 8911

--- sources/MultiEnteros.cpp
+++ sources/MultiEnteros.cpp
@@ -11,13 +11,20 @@ class MultiEnteros{
     int w, x, y, z;
     int pot;
     double r, p, q;
+    //int i=0;
+    int resultado;
+    string strA;
 
     int multiplicacion(int a, int b){
-        tamaño(a, b);
-        cout<<n<<endl;
+        //i++;
+        //cout<<i<<endl;
+        strA = to_string(a);
+        strA = resize(strA);
+        n = strA.size();
+        //cout<<"n: "<<n<<endl;
         if(n<=2){
             return a*b;
-        } else{
+        } else if (n>2){
             s = n/2;
             pot = pow(10, s);
             w = a/pot;
@@ -25,28 +32,29 @@ class MultiEnteros{
             y = b/pot;
             z = b%pot;
             r = multiplicacion(w+x, y+z);
-            //cout<<r<<endl;
-            //p = multiAux(w, y);
-            //q = multiAux(x, z);
-            //return ((p*pow(10, 2*s)) + ((r-p-q)*pow(10, s)) + q);
+            //cout<<"r: "<<r<<endl;
+            p = multiplicacion(w, y);
+            //cout<<"p: "<<p<<endl;
+            q = multiplicacion(x, z);
+            //cout<<"q: "<<q<<endl;
+            return ((p*pow(10, 2*s)) + ((r-p-q)*pow(10, s)) + q);
         }
         return 0;
     }
 
-    void tamaño(int a, int b){
-        string strA = to_string(a);
-        string strB = to_string(b);
-        if(strA.size()>strB.size()){
-            n = strA.size();
-        } else if(strB.size()>strA.size()){
-            n = strB.size();
+    string resize(string mayor){
+        string temp = "0";
+        while(mayor.size()%2!=0){
+            mayor.insert(0, temp);
+            //cout<<mayor<<"."<<endl;
         }
+        return mayor;
     }
 };
 
 int main(){
-    int a = 1234;
-    int b = 981;
+    int a = 8910;
+    int b = 8911;
     int resultado;
     MultiEnteros m;
     resultado = m.multiplicacion(a, b);
